744844c3f32429ad5d5cba18d32a9587c1b313b9
Poorna Kaushalya <poornakashalya80@gmail.com>
2025-05-05 14:13:10 +0530

    Update Folder Sructure

--- backend/src/main/java/com/flavourflow/backend/config/JwtConstant.java
+++
@@ -1,8 +0,0 @@
-package com.flavourflow.backend.config;
-
-public class JwtConstant {
-
-    public static String JWT_HEADER="Authorization";
-    public static String SECRET_KEY="jubajiuhwr4tsvgvsnsuwhuhe2jnnhsbwdfv8sderfvneackprachdsefg8oi56uhbnaxcvbdjws";
-
-}
